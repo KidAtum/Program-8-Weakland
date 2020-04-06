b6175c1ce9f73d9c3378df944cf998cdb289204a
Lucas Weakland <lm.weakland@setonhill.edu>
2020-04-06 16:28:12 -0400

    Ready for review.

--- Program 8 Weakland/ViewController.swift
+++ Program 8 Weakland/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // Second View
+    @IBOutlet weak var outputLabel1: UILabel!
+    @IBAction func bop(_ sender: Any) {
+        outputLabel1.text = "Nice work! Keep Going!"
+    }
+    
+    // Third View
+    @IBOutlet weak var outputLabel2: UILabel!
+    @IBAction func swipe(_ sender: Any) {
+        outputLabel2.text = "Great Work!"
+    }
+    
+    
+    @IBAction func bopit(_ sender: Any) {
+        outputLabel1.text = "Nice work!"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
